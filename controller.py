bce15acd387086979ed9b3175a85d97a72302569
Alex Chan <alexkchan@gmail.com>
2015-02-11 18:09:50 -0800

    Functionality: Can detect new SanDisk servers and add them to the google sheet. Also added new pseudo code

--- controller.py
+++ controller.py
@@ -2,10 +2,14 @@ import subprocess	#To call temrinal commands
 import re			#Regular expression syntax formats
 
 from googleSheet import get_spreadsheet_id_row_mapping
+from googleSheet import add_server_to_new_row
+from googleSheet import check_last_step
+
+CONFIGURE_DONE = 10
 
 def get_server_id_list():
 	#scan for wifi
-	proc = subprocess.Popen('iwlist wlan0 scan 2>/dev/null', shell=True, stdout=subprocess.PIPE,)
+	proc = subprocess.Popen('sudo wlist wlan0 scan 2>/dev/null', shell=True, stdout=subprocess.PIPE,)
 	#displays outputs of list
 	stdout_str = proc.communicate()[0]
 	#parse the outputs
@@ -29,18 +33,38 @@ print(server_ids)
 server_id_row_map = get_spreadsheet_id_row_mapping()
 
 print(server_id_row_map)
+	
+#scan for SanDisk servers, add new row to Google sheet if found
+for server_id in server_ids:
+	if server_id not in server_id_row_map:
+		add_server_to_new_row(server_id)
+	if server_id in server_id_row_map:
+		if check_last_step(server_id) != CONFIGURE_DONE:
+				pass
 
-#Add empty dictionary to keep track active threads, in case server dies partway through
-	# Mapping of server_ids to thread
 
-#Add a dictionary of wifi dongles 
 
-#Mutex threading to control what task the wifi dongles are on
-# for server_id in server_ids:
-# 	if server_id not in server_dictionary:
-		#Method: Look at available WiFi dongles, configure the server on one
 		#Check to see if partway through configuration
-		#Add new row to spreadsheet		
+			#If in spreadsheet, look for last step. Also verify by checking for particular files
+			# 	on the server that indicate which step the configuration is on
+		#Add new row to spreadsheet	if new
+#set of WiPi based on output of ifconfig
+#delete from the list of the WiPi and SanDisk if not available / assigned to thread
+#check to see if any dead threads using enumerate
+	#if dead -- find out the last step / or if it was finished (Rsync for videos?) 	
+	#check if it was complete set to 'none'- if not then display error message
+#Create a new thread forWiPi/SanDisk pair. Then pass in server ID. 
+	#Consider using zip(WiPi Set , SanDisk Set)
+	#For WiPi in Sandisk 
+
+
+
+
+
+
+
+#function:
+# check next step for a particular ID
 
 #choose server/SSID that is not being configured, assign to WiFi dongle
 
--- googleSheet.py
+++ googleSheet.py
@@ -16,7 +16,7 @@ sh = gc.open("SanDisk Configuration Database")
 #Select worksheet by index
 worksheet = sh.get_worksheet(0)
 
-
+ROW_OFFSET = 3
 # ##################################################################################
 # #Searching for a particular server based on MAC 
 # cellSearchResult = worksheet.findall("CDDE")
@@ -27,5 +27,15 @@ worksheet = sh.get_worksheet(0)
 ##################################################################################
 
 def get_spreadsheet_id_row_mapping():
-	ssids = worksheet.col_values(1)[3:]
-	return dict(zip(ssids, range(1, len(ssids) + 3)))
+	ssids = worksheet.col_values(1)[ROW_OFFSET:]
+	return dict(zip(ssids, range(1, len(ssids) + ROW_OFFSET)))
+
+def add_server_to_new_row(ssid):
+	lastRow = (len(worksheet.col_values(1)))+1
+	worksheet.update_cell(lastRow, 1,ssid)
+	# return true #Is there a way to check if the update_cell command worked?
+
+def check_last_step(id):
+	matches = worksheet.findall(id)
+	lastStep = len(worksheet.row_values(matches[0].row))
+	return lastStep
